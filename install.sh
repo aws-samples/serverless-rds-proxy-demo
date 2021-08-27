f23462395e493b54e44dd15afb8da5b534adde85
Pankaj Agrawal <pankaagr@amazon.com>
2021-08-27 14:21:51 +0200

    install artillery

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ sudo apt install awscli
 sudo apt install docker.io
 sudo apt install unzip
 sudo apt install npm
-#sudo usermod -aG docker $USER
-#newgrp docker
+sudo usermod -aG docker $USER
+newgrp docker
 curl -L https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip -o aws-sam-cli-linux-x86_64.zip
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
 sudo ./sam-installation/install
