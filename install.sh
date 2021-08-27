c9ff4b6961ab5f712069ede4852db2670c011bb9
Pankaj Agrawal <pankaagr@amazon.com>
2021-08-27 10:46:48 +0200

    install pre req script on linux

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt install docker.io
+sudo apt install unzip
+sudo usermod -aG docker $USER
+newgrp docker
+curl -L https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip -o aws-sam-cli-linux-x86_64.zip
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install
+which sam
+sam --version
